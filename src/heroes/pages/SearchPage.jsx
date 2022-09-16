cf4361fa7e931e8c0f1e8eb66f71f641b1a206f7
Carlos Motiño <carlos.motino@elcatex.com>
2022-09-16 16:45:12 -0600

    seccion de heroes app

---
+++ src/heroes/pages/SearchPage.jsx
@@ -0,0 +1,97 @@
+import { useNavigate, useLocation } from 'react-router-dom'
+import queryString from 'query-string';
+import { useForm } from '../../hooks/useForm'
+import { HeroCard } from '../components/HeroCard'
+import { getHerosByName } from '../helpers';
+
+
+export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse( location.search )
+
+  const heros = getHerosByName(q);
+
+  const showSearch = (q.length === 0);
+  const showError = (q.length > 0) && heros.length === 0 ;
+
+  const { searchText, onInputChange  } = useForm({
+    searchText: q
+  });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault();
+
+    if (searchText.trim().lenght <= 1 ) return;
+
+    navigate(`?q=${ searchText.toLowerCase().trim() }`);
+
+  }
+
+  return (
+
+    <>
+      <h1>SearchPage</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form onSubmit={ onSearchSubmit }>
+            <input 
+              type="text" 
+              className="form-control" 
+              placeholder='Search a hero'
+              name='searchText'
+              autoComplete='off'
+              value={ searchText }
+              onChange={ onInputChange }
+            />
+
+            <button className="btn btn-outline-primary mt-2">
+              Search
+            </button>
+
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          
+          <div 
+            className={`alert alert-primary animate__animated animate__fadeIn ${ showSearch ? '' : 'd-none' }`}
+          >
+            Search a hero
+          </div>
+
+          <div 
+            className={`alert alert-danger animate__animated animate__fadeIn ${ showError ? '' : 'd-none' }`}
+          >
+            No hero with <b>{q}</b>
+          </div>
+
+          {
+            heros.map( hero => (
+              <HeroCard key={ hero.id } {...hero}/>
+            ))
+          }
+
+          {/* <HeroCard /> */}
+
+
+        </div>
+      </div>
+
+      
+
+
+    </>
+
+    
+  )
+}
